5c347ab10bf025c176e547813a3368bfc196afd8
Pablo Salituri <pablo_salituri@live.com.ar>
2026-05-16 16:22:25 -0300

    Feat: Create routes for API

---
+++ app/api/routes.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from uuid import uuid4
+from datetime import datetime, timezone
+
+from app.models.request_models import QueryRequest
+from app.models.response_models import QueryResponse
+from app.core.enums import CategoryEnum, PriorityEnum
+
+router = APIRouter()
+
+@router.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+@router.post("/query", response_model = QueryResponse)
+def query_endpoint(request: QueryRequest):
+    return QueryResponse(
+        request_id = str(uuid4()),
+        timestamp_utc = datetime.now(timezone.utc).isoformat(),
+        provider_name = "dummy",
+        model_used = "dummy",
+        category = CategoryEnum.GENERAL_INQUIRY,
+        priority = PriorityEnum.LOW,
+        answer = "Temporary mock response.",
+        confidence = 0.0,
+        actinos = []
+    )

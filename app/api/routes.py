d6382d56c5780d410d52ea1a3b12185b2e83b9f9
Pablo Salituri <pablo_salituri@live.com.ar>
2026-05-17 12:04:07 -0300

    Fix: Get keys from openAI response

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,8 +4,9 @@ from datetime import datetime, timezone
 
 from app.models.request_models import QueryRequest
 from app.models.response_models import QueryResponse
-from app.core.enums import CategoryEnum, PriorityEnum
 from app.providers.openai_provider import ask_openai
+from app.services.response_parser import parse_llm_response
+from app.core.config import MODEL_NAME
 
 router = APIRouter()
 
@@ -16,14 +17,17 @@ def health_check():
 
 @router.post("/query", response_model = QueryResponse)
 def query_endpoint(request: QueryRequest):
+    raw_llm_response = ask_openai(request.question)
+    parsed_response = parse_llm_response(raw_llm_response)
+    
     return QueryResponse(
         request_id = str(uuid4()),
         timestamp_utc = datetime.now(timezone.utc).isoformat(),
-        provider_name = "dummy",
-        model_used = "dummy",
-        category = CategoryEnum.GENERAL_INQUIRY,
-        priority = PriorityEnum.LOW,
-        answer = ask_openai(request.question),
-        confidence = 0.0,
-        actions = []
+        provider_name = "openai",
+        model_used = MODEL_NAME,
+        category = parsed_response.category,
+        priority = parsed_response.priority,
+        answer = parsed_response.answer,
+        confidence = parsed_response.confidence,
+        actions = parsed_response.actions
     )

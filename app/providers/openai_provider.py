8e5fc7aa635c3aea673f9bb209635d2fa0ad0a93
Pablo Salituri <pablo_salituri@live.com.ar>
2026-05-17 12:04:37 -0300

    Fix: Config opanAI provider to read from prompt

--- app/providers/openai_provider.py
+++ app/providers/openai_provider.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-
+from app.services.prompt_service import load_main_prompt
 from app.core.config import (
     OPENAI_API_KEY,
     MODEL_NAME,
@@ -7,17 +7,20 @@ from app.core.config import (
     MAX_COMPLETION_TOKENS
 )
 
+
 client = OpenAI(api_key = OPENAI_API_KEY)
 
 
 def ask_openai(question: str) -> str:
+    system_prompt = load_main_prompt()
+    
     response = client.responses.create(
         model = MODEL_NAME,
         temperature = OPENAI_TEMPERATURE,
         max_output_tokens = MAX_COMPLETION_TOKENS,
         
         input = f"""
-            Answer the following customer support question briefly.input
+            {system_prompt}
             
             User question:
             {question}
